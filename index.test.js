683ad47629265ec19a157403a30ad1dd390ad948
Jesse-Tingle <jtingle0@gmail.com>
2020-03-22 19:41:57 -0400

    test for register new user route

---
+++ index.test.js
@@ -0,0 +1,11 @@
+const supertest = require("supertest");
+const server = require("./api/server.js");
+
+test("register route", async () => {
+	const res = await supertest(server)
+		.post("/auth/register")
+		.send({ username: "testUser", password: "12Ob67b6nt5" });
+	expect(res.statusCode).toBe(201);
+	expect(res.type).toBe("application/json");
+	expect(res.body.username).toBe("testUser");
+});
