f6ebd2a8ec2ffdb4342c924605329dfb136329e1
Jesse-Tingle <jtingle0@gmail.com>
2020-03-22 21:46:45 -0400

    reseeding test database each tiime tests are run to prevent false errors from happening

--- index.test.js
+++ index.test.js
@@ -1,5 +1,10 @@
 const supertest = require("supertest");
 const server = require("./api/server.js");
+const db = require("./database/dbConfig.js");
+
+beforeEach(async () => {
+	await db.seed.run();
+});
 
 test("register route", async () => {
 	const res = await supertest(server)
@@ -9,3 +14,5 @@ test("register route", async () => {
 	expect(res.type).toBe("application/json");
 	expect(res.body.username).toBe("testUser");
 });
+
+test("login route", async () => {});
