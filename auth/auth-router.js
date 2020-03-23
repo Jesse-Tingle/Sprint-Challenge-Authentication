ff605bc0ca6ffa96475e0f24c77838454732dc07
Jesse-Tingle <jtingle0@gmail.com>
2020-03-22 20:00:34 -0400

    added authenticate middleware and part of session storage code

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const Users = require("./auth-user-model.js");
 
-router.post('/register', (req, res) => {
-  // implement registration
-});
+router.post("/register", (req, res) => {});
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+	// implement login
 });
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,23 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const sessions = {};
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+function authenticate() {
+	const authError = {
+		message: "You shall not pass!"
+	};
+	return async (req, res, next) => {
+		try {
+			if (!req.session || !req.session.user) {
+				return res.status(401).json(authError);
+			}
+
+			next();
+		} catch (error) {
+			next(error);
+		}
+	};
+}
+
+module.exports = {
+	sessions,
+	authenticate
 };
